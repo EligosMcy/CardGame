d2bfbb53eab48d7aa4e282fc36edb324fb69a6b4
NewVR_Eligos <2284226884@qq.com>
2025-06-03 11:19:03 +0800

    添加 弯曲手牌 和 抽牌效果 还有Bug Hand Spline绘制的错误

--- Assets/Scripts/Views/CardView.cs
+++ Assets/Scripts/Views/CardView.cs
@@ -1,9 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 namespace Assets.Scripts.Views
 {
     public class CardView : MonoBehaviour
     {
+        [SerializeField] private TMP_Text _title;
 
+        [SerializeField] private TMP_Text _description;
+
+        [SerializeField] private TMP_Text _mana;
+
+        [SerializeField] private SpriteRenderer _imageSr;
+
+        [SerializeField] private GameObject _wrapper;
     }
 }
